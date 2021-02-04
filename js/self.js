52d8b932d8b2649d5fdaac1cfcd66af2855dfe2e
yaoli <18861821692@163.com>
2021-02-04 16:59:47 +0800

    update

--- js/self.js
+++ js/self.js
@@ -1,8 +1,1 @@
-// 魔改代码 js
-// var full_page = document.getElementsByClassName("full_page");
-// if (full_page.length != 0) {
-//   full_page[0].style.background = "transparent";
-// }
-// var xiaokang = new xkTool(
-//   "https://ae01.alicdn.com/kf/H21b5f6b8496141a1979a33666e1074d9x.jpg"
-// );
+"use strict";
